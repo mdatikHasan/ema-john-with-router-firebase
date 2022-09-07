1fd77e8b773c56f606c45aea373ca2affa0a25ff
mdatikHasan <atikh2030@gmail.com>
2022-09-07 16:37:06 +0600

    first commit

---
+++ src/_auth_stepsk.js
@@ -0,0 +1,18 @@
+/* 
+    1. Create a new project in console.google.com
+    2. npm install firebase
+    3. create firebase.init.js and import getAuth to export auth 
+    4. Firebase setting > Authentication > enable email and password authentication
+    5. 
+    6. 
+    7. 
+    8. 
+    9. 
+*/
+
+/* * Hosting steps
+    1. npm install -g firebase-tools
+    2. firebase login
+    3. firebase init
+    4. firebase deploy
+*/
